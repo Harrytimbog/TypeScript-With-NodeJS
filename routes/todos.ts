7db39366fe97f488213fa91f3d189dcba53f1d1e
Timilehin Arigbede <Harriemannie@gmail.com>
2022-09-07 13:27:59 +0100

    Finishing the REST Routes

--- routes/todos.ts
+++ routes/todos.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { Todo } from "../models/todos";
 
-const todos: Todo[] = [];
+let todos: Todo[] = [];
 
 const router = Router();
 
@@ -15,6 +15,27 @@ router.post("/todo", (req, res, next) => {
     text: req.body.text,
   };
   todos.push(newTodo);
+  res.status(201).json({ message: "Added Todo", todos: todos });
+});
+
+router.put("/todo/:todoId", (req, res, next) => {
+  const todoId = req.params.todoId;
+  const todoIndex = todos.findIndex((todoItem) => todoItem.id === todoId);
+  if (todoIndex >= 0) {
+    todos[todoIndex] = {
+      id: todos[todoIndex].id,
+      text: req.body.text,
+    };
+    return res.status(200).json({ message: "Updated todo", todos: todos });
+  }
+  res.status(404).json({ message: "Could not find todo for this id" });
+});
+
+router.delete("/todo/:todoId", (req, res, next) => {
+  const todoId = req.params.todoId;
+
+  todos = todos.filter((todoItem) => todoItem.id !== todoId);
+  res.status(200).json({ message: "Deleted todo", todos: todos });
 });
 
 export default router;
